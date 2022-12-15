e771c1de30b4562e379e1ae073a30f2b0ef237b9
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2022-12-16 01:12:41 +0530

    DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const cors = require('cors');
+const colors = require('colors');
 dotenv.config()
+connectDB();
 const app = express();
-const cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
 
@@ -26,5 +29,5 @@ app.get("/api/chats/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Server started at port : ", PORT);
+    console.log(`Server started at port : ${PORT}`.yellow.bold);
 })
