2e8f5238c72f13af26b020d3f11305e6bda8eeef
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2022-12-17 01:10:16 +0530

    middlewares error handlers routes for auth and registeration

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,23 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const cors = require('cors');
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config()
 connectDB();
 const app = express();
-
+// To accept JSON data
+app.use(express.json());
 app.use(cors()) // Use this after the variable declaration
 
 app.get("/", (req, res) => {
     res.send("API is live")
 })
 
-app.get("/api/chats", (req, res) => {
-    res.send(chats);
-})
+app.use("/api/user", userRoutes);
 
-app.get("/api/chats/:id", (req, res) => {
-    const chat = chats.find((chat) => chat._id === req.params.id);
-    if (!chat) {
-        const err = new Error("Chat is invalid");
-        res.status(404).send(err.message);
-    }
-    res.send(chat);
-})
-
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started at port : ${PORT}`.yellow.bold);
